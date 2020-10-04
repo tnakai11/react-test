eb608bc3d510ce2a6739fa450767ecbbbcb06d1c
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2020-10-04 20:44:07 +0900

    Updates

---
+++ precache-manifest.0648f965a60638ed3fe684c02bd3a974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1f7f07b0e7b9408f4e155295d98137",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "302c9c558b2bc31d8e2e",
+    "url": "/react-test/static/css/main.52dff94f.chunk.css"
+  },
+  {
+    "revision": "156dd8d118fa9d47cbef",
+    "url": "/react-test/static/js/2.508cfb54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-test/static/js/2.508cfb54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "302c9c558b2bc31d8e2e",
+    "url": "/react-test/static/js/main.25282ea6.chunk.js"
+  },
+  {
+    "revision": "a73b77d85a0436678cfa",
+    "url": "/react-test/static/js/runtime-main.a3de2790.js"
+  }
+]);
